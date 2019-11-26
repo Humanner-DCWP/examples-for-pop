eff8e34c6a369b2cb5acd209d63e9375370d3920
Leo <losoviz@gmail.com>
2019-11-27 00:24:28 +0800

    Added fieldArg and descriptions

--- src/FieldValueResolvers/HelperFieldValueResolver.php
+++ src/FieldValueResolvers/HelperFieldValueResolver.php
@@ -24,11 +24,38 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
         return $types[$fieldName] ?? parent::getSchemaFieldType($fieldResolver, $fieldName);
     }
 
+    public function getSchemaFieldArgs(FieldResolverInterface $fieldResolver, string $fieldName): array
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'contentMesh':
+                return [
+                    [
+                        SchemaDefinition::ARGNAME_NAME => 'githubRepo',
+                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                        SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('GitHub repository for which to fetch data, in format \'account/repo\' (eg: \'leoloso/PoP\')', 'examples-for-pop'),
+                    ],
+                    [
+                        SchemaDefinition::ARGNAME_NAME => 'weatherZone',
+                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                        SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('Zone from which to retrieve weather data, as listed in https://api.weather.gov/zones/forecast', 'examples-for-pop'),
+                    ],
+                    [
+                        SchemaDefinition::ARGNAME_NAME => 'photoPage',
+                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_INT,
+                        SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('Page from which to fetch photo data', 'examples-for-pop'),
+                    ],
+                ];
+        }
+
+        return parent::getSchemaFieldArgs($fieldResolver, $fieldName);
+    }
+
     public function getSchemaFieldDescription(FieldResolverInterface $fieldResolver, string $fieldName): ?string
     {
         $translationAPI = TranslationAPIFacade::getInstance();
         $descriptions = [
-            'contentMesh' => $translationAPI->__('My user ID (user must be logged in)', 'user-state'),
+            'contentMesh' => $translationAPI->__('Fetch \'content mesh\' data (i.e. data from different services required to power the application), including repository data from GitHub, weather data from the National Weather Service, and random photo data from Unsplash', 'examples-for-pop'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($fieldResolver, $fieldName);
     }
@@ -48,10 +75,13 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
                                     'https://api.github.com/repos/%s',
                                     $fieldArgs['githubRepo'] ?? 'leoloso/PoP'
                                 ),
-                                'weather' => 'https://api.weather.gov/zones/forecast/MOZ028/forecast',
+                                'weather' => sprintf(
+                                    'https://api.weather.gov/zones/forecast/%s/forecast',
+                                    $fieldArgs['weatherZone'] ?? 'MOZ028'
+                                ),
                                 'photos' => sprintf(
                                     'https://picsum.photos/v2/list?page=%s&limit=10',
-                                    rand(1,20)
+                                    $fieldArgs['photoPage'] ?? rand(1, 20)
                                 )
                             ],
                         ]
