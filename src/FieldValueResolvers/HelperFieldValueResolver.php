c6d73ee44609e4f1ec66596316acbcd4a625deae
Leo <losoviz@gmail.com>
2019-11-28 17:25:56 +0800

    Added persisted fields

--- src/FieldValueResolvers/HelperFieldValueResolver.php
+++ src/FieldValueResolvers/HelperFieldValueResolver.php
@@ -1,7 +1,10 @@
 <?php
 namespace Leoloso\ExamplesForPoP\FieldValueResolvers;
 
+use PoP\Engine\Misc\Extract;
+use PoP\ComponentModel\GeneralUtils;
 use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\ComponentModel\Schema\TypeCastingHelpers;
 use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
@@ -12,6 +15,8 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
     public static function getFieldNamesToResolve(): array
     {
         return [
+            'meshServices',
+            'meshServiceData',
             'contentMesh',
         ];
     }
@@ -19,6 +24,8 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
     public function getSchemaFieldType(FieldResolverInterface $fieldResolver, string $fieldName): ?string
     {
         $types = [
+            'meshServices' => TypeCastingHelpers::combineTypes(SchemaDefinition::TYPE_ARRAY, SchemaDefinition::TYPE_URL),
+            'meshServiceData' => SchemaDefinition::TYPE_OBJECT,
             'contentMesh' => SchemaDefinition::TYPE_OBJECT,
         ];
         return $types[$fieldName] ?? parent::getSchemaFieldType($fieldResolver, $fieldName);
@@ -28,6 +35,8 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
     {
         $translationAPI = TranslationAPIFacade::getInstance();
         switch ($fieldName) {
+            case 'meshServices':
+            case 'meshServiceData':
             case 'contentMesh':
                 return [
                     [
@@ -55,7 +64,9 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
     {
         $translationAPI = TranslationAPIFacade::getInstance();
         $descriptions = [
-            'contentMesh' => $translationAPI->__('Fetch \'content mesh\' data (i.e. data from different services required to power the application), including repository data from GitHub, weather data from the National Weather Service, and random photo data from Unsplash', 'examples-for-pop'),
+            'meshServices' => $translationAPI->__('Services required to create a \'content mesh\' for the application: GitHub data for a specific repository, weather data from the National Weather Service for a specific zone, and random photo data from Unsplash', 'examples-for-pop'),
+            'meshServiceData' => $translationAPI->__('Retrieve data from the mesh services', 'examples-for-pop'),
+            'contentMesh' => $translationAPI->__('Retrieve data from the mesh services and create a \'content mesh\'', 'examples-for-pop'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($fieldResolver, $fieldName);
     }
@@ -64,29 +75,56 @@ class HelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolve
     {
         $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
         switch ($fieldName) {
-            case 'contentMesh':
+            case 'meshServices':
+                return [
+                    'github' => sprintf(
+                        'https://api.github.com/repos/%s',
+                        $fieldArgs['githubRepo'] ?? 'leoloso/PoP'
+                    ),
+                    'weather' => sprintf(
+                        'https://api.weather.gov/zones/forecast/%s/forecast',
+                        $fieldArgs['weatherZone'] ?? 'MOZ028'
+                    ),
+                    'photos' => sprintf(
+                        'https://picsum.photos/v2/list?page=%s&limit=10',
+                        $fieldArgs['photoPage'] ?? rand(1, 20)
+                    )
+                ];
+            case 'meshServiceData':
                 return $fieldResolver->resolveValue(
                     $resultItem,
                     $fieldQueryInterpreter->getField(
                         'getAsyncJSON',
                         [
-                            'urls' => [
-                                'github' => sprintf(
-                                    'https://api.github.com/repos/%s',
-                                    $fieldArgs['githubRepo'] ?? 'leoloso/PoP'
-                                ),
-                                'weather' => sprintf(
-                                    'https://api.weather.gov/zones/forecast/%s/forecast',
-                                    $fieldArgs['weatherZone'] ?? 'MOZ028'
-                                ),
-                                'photos' => sprintf(
-                                    'https://picsum.photos/v2/list?page=%s&limit=10',
-                                    $fieldArgs['photoPage'] ?? rand(1, 20)
+                            'urls' => $fieldResolver->resolveValue(
+                                $resultItem,
+                                $fieldQueryInterpreter->getField(
+                                    'meshServices',
+                                    $fieldArgs
                                 )
-                            ],
+                            ),
                         ]
                     )
                 );
+            case 'contentMesh':
+                $meshServiceData = $fieldResolver->resolveValue(
+                    $resultItem,
+                    $fieldQueryInterpreter->getField(
+                        'meshServiceData',
+                        $fieldArgs
+                    )
+                );
+                if (GeneralUtils::isError($meshServiceData)) {
+                    return $meshServiceData;
+                }
+                return [
+                    'weatherForecast' => Extract::getDataFromPath($fieldName, $meshServiceData, 'weather.periods'),
+                    'photoGalleryURLs' => Extract::getDataFromPath($fieldName, $meshServiceData, 'photos.url'),
+                    'githubMeta' => [
+                        'description' => Extract::getDataFromPath($fieldName, $meshServiceData, 'github.description'),
+                        'starCount' => Extract::getDataFromPath($fieldName, $meshServiceData, 'github.stargazers_count'),
+                    ],
+                ];
         }
 
         return parent::resolveValue($fieldResolver, $resultItem, $fieldName, $fieldArgs);
