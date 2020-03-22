b76649b9121caefa646d9ced9f6aa315d9313f83
Leo <losoviz@gmail.com>
2020-03-22 21:33:07 +0800

    Modified deprecation message

--- src/FieldResolvers/RootFieldResolver.php
+++ src/FieldResolvers/RootFieldResolver.php
@@ -18,10 +18,10 @@ class RootFieldResolver extends Root_Version_0_1_0_FieldResolver
     public function getSchemaFieldDeprecationDescription(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
     {
         // If the query doesn't specify what version of the field to use, add a deprecation message
-        if (!$fieldArgs['versionConstrait']) {
+        if (!$fieldArgs['versionConstraint']) {
             $translationAPI = TranslationAPIFacade::getInstance();
             $deprecationMessage = sprintf(
-                $translationAPI->__('Field \'%1$s\' has a new version: \'%2$s\'. This version will become the default one on January 1st. We advise you to use this new version already, test that it works fine, and report to us if you find any problem. To do the switch, please add the \'versionConstrait\' field argument to your query: %1$s(versionConstrait:\'%2$s\'). If you are unable to switch to the new version, please make sure to explicitly point to the current version \'%3$s\' before January 1st: %1$s(versionConstrait:\'%3$s\'). In case of doubt, please contact us at name@company.com.', 'examples-for-pop'),
+                $translationAPI->__('Field \'%1$s\' has a new version: \'%2$s\'. This version will become the default one on January 1st. We advise you to use this new version already, test that it works fine, and report to us if you find any problem. To do the switch, please add the \'versionConstraint\' field argument to your query: %1$s(versionConstraint:\'%2$s\'). If you are unable to switch to the new version, please make sure to explicitly point to the current version \'%3$s\' before January 1st: %1$s(versionConstraint:\'%3$s\'). In case of doubt, please contact us at name@company.com.', 'examples-for-pop'),
                 $fieldName,
                 '0.2.0',
                 '0.1.0'
