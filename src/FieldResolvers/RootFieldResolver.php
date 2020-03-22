cc8cfedcb25bfe7b88bd88f18f6ecc3b577bfdd5
Leo <losoviz@gmail.com>
2020-03-22 21:54:28 +0800

    Improved deprecation messages

--- src/FieldResolvers/RootFieldResolver.php
+++ src/FieldResolvers/RootFieldResolver.php
@@ -20,15 +20,26 @@ class RootFieldResolver extends Root_Version_0_1_0_FieldResolver
         // If the query doesn't specify what version of the field to use, add a deprecation message
         if (!$fieldArgs['versionConstraint']) {
             $translationAPI = TranslationAPIFacade::getInstance();
-            $deprecationMessage = sprintf(
-                $translationAPI->__('Field \'%1$s\' has a new version: \'%2$s\'. This version will become the default one on January 1st. We advise you to use this new version already, test that it works fine, and report to us if you find any problem. To do the switch, please add the \'versionConstraint\' field argument to your query: %1$s(versionConstraint:\'%2$s\'). If you are unable to switch to the new version, please make sure to explicitly point to the current version \'%3$s\' before January 1st: %1$s(versionConstraint:\'%3$s\'). In case of doubt, please contact us at name@company.com.', 'examples-for-pop'),
-                $fieldName,
-                '0.2.0',
-                '0.1.0'
-            );
             $descriptions = [
-                'userServiceURLs' => $deprecationMessage,
-                'userServiceData' => $deprecationMessage,
+                'userServiceURLs' => sprintf(
+                    $translationAPI->__('Field \'%1$s\' has a new version: \'%2$s\'. This version will become the default one on January 1st. We advise you to use this new version already and test that it works fine; if you find any problem, please report the issue in %3$s. To do the switch, please add the \'versionConstraint\' field argument to your query, using Composer\'s semver constraint rules (%4$s): %1$s(versionConstraint:"%5$s"). If you are unable to switch to the new version, please make sure to explicitly point to the current version \'%6$s\' before January 1st: %1$s(versionConstraint:"%6$s"). In case of doubt, please contact us at name@company.com.', 'examples-for-pop'),
+                    $fieldName,
+                    '0.2.0',
+                    'https://github.com/mycompany/myproject/issues',
+                    'https://getcomposer.org/doc/articles/versions.md',
+                    '^0.2',
+                    '0.1.0'
+                ),
+                'userServiceData' => sprintf(
+                    $translationAPI->__('Field \'%1$s\' has more than 1 version. Please add the \'versionConstraint\' field argument to your query to indicate which version to use (using Composer\'s semver constraint rules: %2$s). To use the latest version, use: %1$s(versionConstraint:"%3$s"). Available versions: \'%4$s\'.', 'examples-for-pop'),
+                    $fieldName,
+                    'https://getcomposer.org/doc/articles/versions.md',
+                    '^0.2',
+                    implode(
+                        $translationAPI->__('\', \'', 'examples-for-pop'),
+                        ['0.2.0', '0.1.0']
+                    )
+                ),
             ];
             return $descriptions[$fieldName] ?? parent::getSchemaFieldDeprecationDescription($typeResolver, $fieldName, $fieldArgs);
         }
